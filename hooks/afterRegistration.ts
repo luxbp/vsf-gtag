5734572b9ec8f484bd1eff42ca1000c6829d0b47
Brian McDonald <bmcdonald@luxbp.com>
2020-12-02 16:41:17 -0500

    feat(store-dimension): add custom store code dimension

--- hooks/afterRegistration.ts
+++ hooks/afterRegistration.ts
@@ -11,9 +11,24 @@ import PromotionClicks from '../subscribers/PromotionClicks';
 import CategoryImpressionSubscriber from '../subscribers/custom/CategoryImpressionSubscriber';
 import CheckoutFunnelSubscriber from '../subscribers/CheckoutFunnelSubscriber';
 import CartStateSubscriber from '../subscribers/custom/CartStateSubscriber';
+import { currentStoreView } from '@vue-storefront/core/lib/multistore';
 
 export function afterRegistration ({Vue, config, store, isServer}) {
   if (config.analytics.id && !isServer) {
+    const view = currentStoreView();
+    if (config.analytics.custom_map) {
+      Vue.prototype.gtag('config', 'GA_MEASUREMENT_ID', {
+        'custom_map': {
+          'dimension1': 'Store Code',
+          ...(config.analytics.custom_map || {})
+        }
+      });
+
+      Vue.prototype.gtag('event', 'store_code_dimension', {
+        'Store Code': view.storeCode
+      });
+    }
+
     let subscribers = [
       RouteChangeSubscriber,
       CategoryImpressionSubscriber,
